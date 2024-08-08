7889d751828d38c45fb1401e6d3245f222ab7a26
Adel Sevastianov <adelsevastanov@gmail.com>
2024-08-08 11:34:35 +0300

    add: gui for game

--- gui/cli/cli.c
+++ gui/cli/cli.c
@@ -1,1 +1,97 @@
 #include "cli.h"
+
+void init_curses() {
+  initscr();
+  keypad(stdscr, TRUE);
+  nodelay(stdscr, TRUE);
+  raw();
+  noecho();
+  cbreak();
+  curs_set(0);
+  start_color();
+  init_pair(1, COLOR_BLACK, COLOR_WHITE);
+  init_pair(2, COLOR_RED, COLOR_GREEN);
+  init_pair(3, COLOR_RED, COLOR_BLUE);
+  init_pair(4, COLOR_WHITE, COLOR_BLACK);  // For help and border
+  init_pair(5, COLOR_BLACK, COLOR_WHITE);
+  init_pair(6, COLOR_YELLOW, COLOR_BLACK);  // Score and Level
+}
+
+WinStruct *init_windows() {
+  WinStruct *window = (WinStruct *)malloc(sizeof(WinStruct));
+  if (window == NULL) {
+    return NULL;
+  }
+  window->field = newwin(22, 22, 2, 14);    // Main game area
+  window->stats = newwin(5, 20, 2, 36);   // Score and level display
+  window->next = newwin(7, 14, 10, 36);   // Next piece display
+  window->help = newwin(10, 20, 17, 36);  // Help display
+  return window;
+}
+
+void draw_board(GameInfo_t *game, WINDOW *win) {
+  box(win, 0, 0);
+  for (int row = 0; row < HEIGHT; row++) {
+    for (int col = 0; col < WIDTH; col++) {
+      if (game->field[row][col] == 0) {
+        mvwprintw(win, row + 1, 2 * col + 1, "%c", ' ');
+        mvwprintw(win, row + 1, 2 * col + 2, "%c", ' ');
+      } else if (game->field[row][col] == 1) {
+        mvwprintw(win, row + 1, 2 * col + 1, "[");
+        mvwprintw(win, row + 1, 2 * col + 2, "]");
+      } else if (game->field[row][col] == 2) {
+        mvwprintw(win, row + 1, 2 * col + 1, "[");
+        mvwprintw(win, row + 1, 2 * col + 2, "]");
+      }
+    }
+  }
+  wrefresh(win);
+}
+
+void draw_stats(GameInfo_t *game, WINDOW *stats) {
+  werase(stats);
+  box(stats, 0, 0);
+  wbkgd(stats, COLOR_PAIR(6));
+  mvwprintw(stats, 1, 1, "Stats");
+  mvwprintw(stats, 2, 1, "Score: %d", game->score);
+  mvwprintw(stats, 3, 1, "Level: %d", game->level);
+  wrefresh(stats);
+}
+
+void draw_next(GameInfo_t *game, WINDOW *next) {
+  werase(next);
+  box(next, 0, 0);
+  mvwprintw(next, 0, 1, "Next");
+  for (int row = 0; row < BLOCK_SIZE; row++) {
+    for (int col = 0; col < BLOCK_SIZE; col++) {
+      if (game->next[row][col] == 1) {
+        mvwprintw(next, row + 2, 2 * col + 1, "[");
+        mvwprintw(next, row + 2, 2 * col + 2, "]");
+      } else {
+        mvwprintw(next, row + 2, 2 * col + 1, " ");
+        mvwprintw(next, row + 2, 2 * col + 2, " ");
+      }
+    }
+  }
+  wrefresh(next);
+}
+
+void draw_help(WINDOW *help) {
+  werase(help);
+  box(help, 0, 0);
+  mvwprintw(help, 1, 1, "Help");
+  mvwprintw(help, 2, 1, "Left:      <-");
+  mvwprintw(help, 3, 1, "Right:     ->");
+  mvwprintw(help, 4, 1, "Down:      v");
+  mvwprintw(help, 5, 1, "Rotate:    space");
+  mvwprintw(help, 6, 1, "Quit:      q");
+  mvwprintw(help, 7, 1, "Pause:     p"); // dose not working //
+  wrefresh(help);
+}
+
+void draw_frontend(GameInfo_t *game, WinStruct *window) {
+  draw_board(game, window->field);
+  draw_stats(game, window->stats);
+  draw_next(game, window->next);
+  draw_help(window->help);
+}
--- gui/cli/cli.h
+++ gui/cli/cli.h
@@ -1,15 +1,34 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <math.h>
 #include <ncurses.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "../../brick_game/tetris/tetris.h"
 
 typedef struct {
-  WINDOW *win;
-  WINDOW *preview;
-  WINDOW *addinfo;
+  WINDOW *field;
+  WINDOW *stats;
+  WINDOW *next;
+  WINDOW *help;
 } WinStruct;
+
+void init_curses();
+void draw_board(GameInfo_t *field, WINDOW *win);
+void draw_next(GameInfo_t *game, WINDOW *nextfigure);
+void draw_addinfo(GameInfo_t *game, WINDOW *addinfo);
+void draw_stats(GameInfo_t *game, WINDOW *stats);
+void draw_help(WINDOW *help);
+void draw_next(GameInfo_t *game, WINDOW *next);
+void draw_gameover(WinStruct *window);
+void draw_frontend(GameInfo_t *game, WinStruct *window);
+void draw_pause(WinStruct *window);
+int random_color();
+WinStruct *init_windows();
 
 #endif
